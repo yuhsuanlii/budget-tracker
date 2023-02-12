c6515459025015f1ce12a10b0a441e5dc96d8b11
yuhsuanlii <charlie9684@gmail.com>
2023-02-12 14:39:44 +0800

    feat: add router path

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import { BudgetTrackerProvider } from "./hooks/useBudgetTracker";
 import KeeperPage from "./pages/keeperPage";
 import TrackerPage from "./pages/trackerPage";
+import Navbar from "./components/navbar";
 
 const App = () => {
     return (
         <BudgetTrackerProvider>
             <Routes>
                 <Route path="/" element={<KeeperPage />} />
+                <Route path="/user" element={<Navbar />} />
                 <Route path="/keeper" element={<KeeperPage />} />
                 <Route path="/tracker" element={<TrackerPage />} />
+                <Route path="/chart" element={<Navbar />} />
+                <Route path="/logout" element={<Navbar />} />
             </Routes>
         </BudgetTrackerProvider>
     )
